e36721fca0cfc0aaf9b4a14b4f5c877d138f4ca7
Martin <70632679+martinbolle@users.noreply.github.com>
2024-09-23 13:50:02 +0200

    Update of bentogrid

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -50,6 +50,9 @@ export const BentoGridItem = ({
   spareImg?: string;
 }) => {
 
+  const leftLists = ["ReactJS", "Remix", "Typescript"];
+  const rightLists = ["NextJS", "NuxtJS", "GraphQL"];
+
   const [copied, setCopied] = useState(false);
   const handleCopy = () => {
     navigator.clipboard.writeText("martin.bolle97@hotmail.com")
@@ -107,26 +110,34 @@ export const BentoGridItem = ({
 
         {/*Tech stack*/}
         {id === 3 && (
-
-          <div className="flex gap-1 lg:gap-5 w-fit absolute right-1 lg:right-1">
-            <div className="flex flex-col gap-3 lg:gap-8">
-              {['React.js', 'Next.js', 'TypeScript'].map((item, index) => (
-                <span key={item} className="py-2 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity:100 rounded-lg text-center bg-[#10132E] ">
-                {item}
-               </span> 
-              ))}
-            <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]"/>
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+              {/* tech stack lists */}
+              <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+                {leftLists.map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+              </div>
+              <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+                {rightLists.map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+              </div>
             </div>
-            <div className="flex flex-col gap-3 lg:gap-8">
-            <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]"/>
-              {['Sanity', 'Remix', 'Tailwind'].map((item, index) => (
-                <span key={item} className="py-2 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity:100 rounded-lg text-center bg-[#10132E] ">
-                {item}
-               </span> 
-              ))}
-            </div>
-          </div>
-        )}
+          )}
         {id === 6 && (
           <div className="mt-5 relative">
             <div className={`absolute -bottom-5 right-0`}>
